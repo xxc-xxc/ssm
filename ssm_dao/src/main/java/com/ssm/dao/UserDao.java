c192dfa5e8fdfae99eb8fad1a4103ad43b76a66b
xxc-xxc <55835158+xxc-xxc@users.noreply.github.com>
2020-12-28 15:01:59 +0800

    建立模块之间的关联关系

---
+++ ssm_dao/src/main/java/com/ssm/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.ssm.dao;
+
+/**
+ * 用户持久层接口
+ */
+public interface UserDao {
+
+    int save();
+
+}
--- ssm_domain/src/main/java/com/ssm/domain/User.java
+++ ssm_domain/src/main/java/com/ssm/domain/User.java
@@ -1,12 +1,16 @@
 package com.ssm.domain;
 
+import java.beans.Transient;
+import java.io.Serializable;
+
 /**
  * @Author xxc
  * @Date 2020/12/28 14:43
  * @Description 用户实体类
  */
-public class User {
-
+public class User implements Serializable {
+    
+    private static final long serialVersionUID = -4478075092323206374L;
     private Integer id;
     private String username;
 
