e85c217a35b860903ad5d17c6ad2a0e24c1dc583
xxc-xxc <55835158+xxc-xxc@users.noreply.github.com>
2020-12-28 18:13:32 +0800

    测试mybatis在web工程能独立运行

--- ssm_web/src/test/java/com/ssm/test/UserDaoTest.java
+++ ssm_web/src/test/java/com/ssm/test/UserDaoTest.java
@@ -1,8 +1,17 @@
 package com.ssm.test;
 
+import com.ssm.dao.UserDao;
+import com.ssm.domain.User;
 import com.ssm.service.UserService;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @Author xxc
@@ -12,10 +21,25 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class UserDaoTest {
 
     @Test
-    public void testUserDao() {
+    public void testUserDao01() {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserService userService = context.getBean("userService", UserService.class);
         userService.findAll();
+    }
+
+    @Test
+    public void testUserDao02() throws IOException {
+        User user = new User();
+        user.setUsername("xxx");
+
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = factory.openSession();
+        UserDao userDao = session.getMapper(UserDao.class);
+        userDao.save(user);
+        session.commit();
+        session.close();
+        inputStream.close();
     }
 
 }
